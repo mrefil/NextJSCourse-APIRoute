1dbdae3df4b389026770a2edf916fe6a931d6b70
Mustafa E <61510151+mrefil@users.noreply.github.com>
2022-01-01 19:02:27 -0500

    Creating & Using Dynamic API Routes

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,13 +1,31 @@
+import { useState } from 'react';
+import { Fragment } from 'react/cjs/react.production.min';
 import {buildFeedbackPath, extractFeedback} from '../api/feedback';
 
 function FeedbackPage(props) {
+    const [feedbackData, setFeedbackData] = useState();
+
+    function loadFeedbackHandler(id) {
+        fetch('/api/' + id)
+        .then((response) => response.json())
+        .then((data) => {
+            setFeedbackData(data.feedback);
+        })
+        // fetch(`/api/${id}`);
+    }
     return (
-        <ul>
-            {props.feedbackItems.map((item) => (
-                <li key={item.id}>{item.text}</li>
-            ))}
-        </ul>
-    )
+        <Fragment>
+            {feedbackData && <p>{feedbackData.email}</p>}
+            <ul>
+                {props.feedbackItems.map((item) => (
+                    <li key={item.id}>{item.text} 
+                        <button onClick={loadFeedbackHandler.bind(null, item.id)}>Show Details</button>
+                    </li>
+                ))}
+            </ul>
+        </Fragment>
+
+    );
 }
 
 export async function getStaticProps() {
